f86468b18ad6e9a06903fb37901538e4af50ed0f
HaritsTama <nayotama1818@gmail.com>
2025-11-27 16:27:29 +0700

    Mengupdate ContainerApp.kt di dalam package repositori dengan menambahkan class baru

--- app/src/main/java/com/example/a141_roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/a141_roomdatabase/repositori/ContainerApp.kt
@@ -1,5 +1,6 @@
 package com.example.a141_roomdatabase.repositori
 
+import android.app.Application
 import android.content.Context
 import com.example.a141_roomdatabase.room.DatabaseSiswa
 
@@ -13,5 +14,13 @@ class ContainerDataApp(private val context : Context):
         OfflineRepositoriSiswa(
             DatabaseSiswa.getDatabase(context).siswaDao()
         )
+    }
+}
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
     }
 }
