fadf14a8df0974b8e081059a6fb979ffd94a462e
HaritsTama <nayotama1818@gmail.com>
2025-11-27 16:05:57 +0700

    Menambahkan Siswa.kt dan SiswaDao.kt di dalam package room(model)

---
+++ app/src/main/java/com/example/a141_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,15 @@
+package com.example.a141_roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConfilct = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa : Siswa)
+}
